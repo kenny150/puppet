a7de318048b701ad00458e36c7846daabe392a0f
Kennedy Alves <alvkennedy@gmail.com>
2017-01-10 18:37:22 -0500

    Colocando nginx para inicialização automatica

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -2,6 +2,6 @@
 class nginx { 
 	package { 'httpd': ensure => absent, }
 	package { 'nginx': ensure => installed, require => Package['httpd'], }
-	service { 'nginx': ensure => running, require => Package['nginx'], }
+	service { 'nginx': ensure => running, require => Package['nginx'], enable => true, }
 	file 	{ '/etc/nginx/default.d/default': ensure => present, source => 'puppet:///modules/nginx/cat-pictures.conf', notify => Service['nginx'], }
 }
