195c569bff3c9ab0d7c9f3a4959d867b0edbe5e2
kenny150 <alvkennedy@gmail.com>
2017-01-14 13:11:38 -0500

    Adcionando usuário art

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,7 +1,8 @@
 node 'puppet-client' { 
+	include nginx
 	file    { '/tmp/hello': content	=> "Hello, world\nThis is my nodes.pp file\n", }
 	#package { 'nginx':      ensure	=> installed, }
-	include nginx
+	user 	{ 'art': ensure => present, comment => "Art Vanderley", home => '/home/art', managehome => true, }
 }
 node 'puppet-client2' { 
 	include nginx
