8b5daa9f51219ae9984d4e233d1663d489db9440
Kennedy Alves <alvkennedy@gmail.com>
2017-01-18 17:46:14 -0200

    Adicionando recurso exec

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -5,6 +5,7 @@ node 'puppet-client' {
 	file    { '/tmp/hello': content	=> "Hello, world\nThis is my nodes.pp file\n", }
 	user 	{ 'kalves': ensure => present, comment => "Kennedy Alves", home => '/home/kalves', managehome => true, }
 	ssh_authorized_key { 'kalves': user => 'kalves', type => 'rsa', key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1", }
+	exec	{ 'Executando comando': command => 'echo Executei este comando', path => '/usr/bin:/usr/sbin:/bin'; }
 }
 node 'puppet-client2' { 
 	include nginx
@@ -12,8 +13,8 @@ node 'puppet-client2' {
 	include sudoers
         user    { 'kalves': ensure => present, comment => "Kennedy Alves", home => '/home/kalves', managehome => true, }
         ssh_authorized_key { 'kalves': user => 'kalves', type => 'rsa', key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1", }
-        #Para remover o acesso temporariamente do usuário
+	exec	{ 'Executando comando': command => 'echo "Executei este comando " > /tmp/output.txt', path => '/usr/bin:/usr/sbin:/bin'; }
         #ssh_authorized_key { 'kalves': user => 'kalves', type => 'rsa', key => "", }
-
+        #Para remover o acesso temporariamente do usuário
 
 }
