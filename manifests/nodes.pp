3c23d282a6bfb650e048f274584211adc7d6fb31
Kennedy Alves <alvkennedy@gmail.com>
2017-01-18 14:29:35 -0200

    Removendo linha da instalação do pacote do nginx

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,7 +1,6 @@
 node 'puppet-client' { 
 	include nginx
 	file    { '/tmp/hello': content	=> "Hello, world\nThis is my nodes.pp file\n", }
-	#package { 'nginx':      ensure	=> installed, }
 	user 	{ 'kalves': ensure => present, comment => "Kennedy Alves", home => '/home/kalves', managehome => true, }
 	ssh_authorized_key {'kalves': user => 'kalves', type => 'rsa', key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1 kalves@centos7-lab', }
 }
