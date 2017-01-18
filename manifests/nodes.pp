3b46be88421107979c0471cf9a33e31b84992bd9
Usuário dono do git <git@puppet-server>
2017-01-18 12:45:00 -0500

    Adicionando comentario

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -8,6 +8,8 @@ node 'puppet-client2' {
 	include nginx
         user    { 'kalves': ensure => present, comment => "Kennedy Alves", home => '/home/kalves', managehome => true, }
         ssh_authorized_key { 'kalves': user => 'kalves', type => 'rsa', key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1", }
+        #Para remover o acesso temporariamente do usuário
+        #ssh_authorized_key { 'kalves': user => 'kalves', type => 'rsa', key => "", }
 
 
 }
