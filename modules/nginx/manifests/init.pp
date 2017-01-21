4d24f9fabf55fd4981224d1accc8b10f7c6e7328
Kennedy Alves <alvkennedy@gmail.com>
2017-01-21 17:35:21 -0200

    Comentando receitas

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,17 +1,22 @@
 # Classe para instalação do nginx
+# Nome da classe
 class nginx { 
+# Remove o pacote httpd
 	package { 'httpd': 
 		ensure => absent,
 	}
+# Instala o pacote do nginx
 	package { 'nginx': 
 		ensure => installed,
 		require => Package['httpd'],
 	}
+# Diz que o servico deve estar rodando e iniciando automaticamente junto com o sistema
 	service { 'nginx': 
 		ensure => running, 
 		require => Package['nginx'], 
 		enable => true,
 	}
+# Remove o arquivo ../default.d/default
 	file 	{ '/etc/nginx/default.d/default': 
 		ensure => absent, 
 	}
--- modules/nginx/manifests/website.pp
+++ modules/nginx/manifests/website.pp
@@ -1,14 +1,23 @@
-#Gerenciado pelo puppet
+# Cria um definition type
 
 define nginx::website ( $site_domain ) {
+
+# Inclui a classe do nginx
 include nginx
+
+# Declara o valor da variável site_name que receberá o valor de cat-pictures (valor do nome do resource type)
 $site_name = $name
+
+# Declara que dentro de conf.d/ deve haver o um arquivo com o valor da variavel .conf , buscando do template
 file { "/etc/nginx/conf.d/${site_name}.conf":
 	content => template('nginx/vhost.conf.erb'),
 	notify	=> Service['nginx'],
 }
+
+# Deve criar o diretório /var/www/ de acordo com o nome da variavel
 file { "/var/www/${name}":
 	ensure 	=> 'directory',
 	recurse	=> true,
 }
+
 }
