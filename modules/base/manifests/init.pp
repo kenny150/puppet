2709907616c1e05feb72aaf1f5be80bfb2e2a330
Usuário dono do git <git@puppet-server>
2017-01-24 19:19:27 -0200

    Configurando padrão de /etc/hosts e resolv.conf

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -18,6 +18,20 @@ service	{ 'firewalld':
 package	{ 'iptables-services':
 	ensure	=> absent,
 	}
+file 	{ '/etc/hosts':
+	ensure	=> present,
+	content	=> template('base/hosts.conf.erb'),
+	mode	=> 0664,
+	owner	=> 'root',
+	group	=> 'root',
+	}
+file 	{ '/etc/resolv.conf':
+	ensure	=> present,
+	source	=> 'puppet:///modules/base/resolv.conf',
+	mode 	=> 0644,
+	owner	=> 'root',
+	group	=> 'root',
+	}
 } 
 default:  { notify{ 'Sistema operacional é Debian': }
 	}
