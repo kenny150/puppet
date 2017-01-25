831c2c57d78da5398fb71398f318dba90e790294
Usuário dono do git <git@puppet-server>
2017-01-25 21:21:09 -0200

    Ajustando receita no puppet-client

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -9,9 +9,16 @@ base::rede { 'unset':
 }	
 
 include nginx
+
 $site_name = 'cat-pictures'
 $site_domain = 'cat-pictures.com'
 $ntp_server = '192.168.0.100'
+
+file	{ '/etc/nginx/conf.d/cat-pictures.conf':
+	content => template('nginx/vhost.conf.erb'),
+        notify  => Service['nginx'],
+        }
+
 nginx::website { 'adorable-animals':
 	site_domain => 'adorable-animals.com',
 }
@@ -36,42 +43,29 @@ ssh::users { 'kalves':
                 key     => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1",
         }
 ssh::users { 'git':
-#                key     => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCa0sRUIlc8bpwTTWbfovipRF8SHs+A3kikDc50egIbNCWBHGWc6JhLqiDd5EBaYHOBg3z39pdkTPxfwSR1K4FO5Ieo/cQdhY1u2OXyxMX9wGt1J7+74w/XLuHwTclXrXeOyz0CXBbgArdhPa9Nw0eZ/Y8kzmwugQSfDBUCQcg5NWeOFfMIx/qX62oPa1U/J3Pl9Xs2LUTuMtS0UBg5zddm9TtbmMtdR9t9TyymN9p1gAYVeNNihIOjQ+5GN4cBmBLwpaQWa3119UEpXlLzTi6YyRBLMubJlznXcvrbXbIaDR7mlufngE0UcQ+MIIuMInEHE5ICeCr6tVcoztBcuJpf",
                 key     => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1",
 	}
-# Executando comando apenas para teste do resource type
-	exec	{ 'Executando comando':
-		command => 'echo Executei este comando', 
-		path 	=> '/usr/bin:/usr/sbin:/bin',
+exec	{ 'Executando comando':
+	command => 'echo Executei este comando', 
+	path 	=> '/usr/bin:/usr/sbin:/bin',
 	}
-# Criando uma cron para fazer um backup do nginx sempre as 4 da manhã
-        cron 	{ 'backup-nginx': 
-		command => '/usr/bin/rsync -avz /etc/nginx/ /etc/nginx-backup/',
-		hour 	=> '04',
-		minute 	=> '00', 
+cron 	{ 'backup-nginx': 
+	command => '/usr/bin/rsync -avz /etc/nginx/ /etc/nginx-backup/',
+	hour 	=> '04',
+	minute 	=> '00', 
 	}
-# Criando uma cron para fazer o sync com o repositório
-	cron 	{ 'git-pull': 
-		command => '/usr/local/bin/pull-updates',
-		minute 	=> '*/10',
-		hour	=> '*',
-		user	=> 'git',
+cron 	{ 'git-pull': 
+	command => '/usr/local/bin/pull-updates',
+	minute 	=> '*/10',
+	hour	=> '*',
+	user	=> 'git',
 	}
-# Criando o arquivo cat-pictures.conf baseado no template vhost.conf.erb
-	file	{ '/etc/nginx/conf.d/cat-pictures.conf':
-		content => template('nginx/vhost.conf.erb'),
-		notify	=> Service['nginx'],
-	}
-# Criando arquivo de teste
-	file 	{ '/tmp/teste':
-		content => 'Testando',
-	}
-# Criando frase de boas vindas
-	file { '/etc/motd' :
-        	content => "Enjoy the server =)\nPUoppet is on the control here\n",
-        }
+file { '/etc/motd' :
+       	content => "Enjoy the server =)\nPUoppet is on the control here\n",
+     }
 
-} #Encerrando a declaração do node puppet-client2
+}
+
 
 ##############################################################################################
 # Receitas para o node puppet-client
