700dfff2a6067615367abffa0ffcf14f5ac9b7e5
Usuário dono do git <git@puppet-server>
2017-01-26 01:05:58 -0200

    Correção de bugs no resource type ssh_authorized_key

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -85,7 +85,7 @@ user	{ 'admin':
 ssh_authorized_Key	{ 'admin':
 	type	=> 'rsa',
 	key	=> "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1",
-	user	=> "$name",
+	user	=> 'admin',
 }
 
 }
