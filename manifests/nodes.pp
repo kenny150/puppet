19d8392fa5d7d8f5336aaf6f4496d8c7dcc1d64e
Usuário dono do git <git@puppet-server>
2017-01-26 00:43:49 -0200

    Adicionando usuario padrão na receita base

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,29 +2,10 @@
 
 node 'puppet-client' { 
 
-if $ipaddress =~ /192.168.*/ {
-	$environment = 'production'
-	notify	{ "Ambiente = $environment": }
-}
-elsif $ipaddress =~ /172.16.*/ {
-	$environment = 'stagging'
-	notify	{ "Ambiente = $environment": }
-}
-else { 
-	$environment = 'dev'
-	notify	{ "Ambiente = $environment": }
-}
-
 include base
 base::rede { 'unset':
 	nome_interface => 'enp0s8',
 }	
-
-if $hostname =~ /web-*/ {
-	include nginx
-}
-else { notify { "Hostname = $hostname": }
-} 
 
 include nginx
 
@@ -49,7 +30,6 @@ file    { '/etc/nginx/conf.d/cat-pictures.conf':
         notify  => Service['nginx'],
 }
 
-
 ssh::newuser	{ 'kalves': 
 	comment 	=> "Kennedy Alves", 
 }
@@ -62,82 +42,33 @@ ssh::users { 'kalves':
         }
 ssh::users { 'git':
                 key     => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1",
-	}
+}
 exec	{ 'Executando comando':
 	command => 'echo Executei este comando', 
 	path 	=> '/usr/bin:/usr/sbin:/bin',
-	}
+}
 cron 	{ 'backup-nginx': 
 	command => '/usr/bin/rsync -avz /etc/nginx/ /etc/nginx-backup/',
 	hour 	=> '04',
 	minute 	=> '00', 
-	}
+}
 cron 	{ 'git-pull': 
 	command => '/usr/local/bin/pull-updates',
 	minute 	=> '*/10',
 	hour	=> '*',
 	user	=> 'git',
-	}
+}
 file { '/etc/motd' :
        	content => "Enjoy the server =)\nPUoppet is on the control here\n",
-     }
+}
 
 }
 
 
 ##############################################################################################
-# Receitas para o node puppet-client
-node 'puppet-client2' { 
-# Definindo valores de variaveis 	
-	$site_name = 'dog-pictures'
-	$site_domain = 'dog-pictures.com'
-# Importando modulos
-	include nginx
-	include ssh
-	include sudoers
-# Criando um arquivo em /tmp/ com o conteúdlo hello, world ...
-	file    { '/tmp/hello':
-                content => "Hello, world\nThis is my nodes.pp file\n",
-        }
-# Criando usuário kalves        
-	user    { 'kalves':
-                ensure 		=> present,
-                comment 	=> "Kennedy Alves",
-                home 		=> '/home/kalves',
-                managehome 	=> true,
-        }
-# Declarando chave de acesso
-        ssh_authorized_key { 'kalves':
-                user 	=> 'kalves',
-                type 	=> 'rsa',
-                key 	=> "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1", 
-        }
-        #ssh_authorized_key { 'kalves': user => 'kalves', type => 'rsa', key => "", }
-        #Para remover o acesso temporariamente do usuário
-# Executando comando de teste
-        exec    { 'Executando comando': 
-                command => 'echo Executei este comando',  
-                path 	=> '/usr/bin:/usr/sbin:/bin', 
-        }
-# Declarando um cron para fazer backup do nginx as 04 da manhã
-        cron    { 'backup-nginx': 
-                command => '/usr/bin/rsync -avz /etc/nginx/ /etc/nginx-backup/', 
-                hour 	=> '04', 
-                minute 	=> '00', 
-	}
-# Declarando um job para atualizar o repositório 
-	cron 	{ 'git-pull': 
-		command => '/usr/local/bin/pull-updates',
-		minute 	=> '*/10',
-		hour	=> '*',
-		user	=> 'git',
-	}
-# Criando o arquivo dog-pictures baseado no template vhost.conf.erb
-	file	{ '/etc/nginx/conf.d/dog-pictures.conf':
-		content	=> template('nginx/vhost.conf.erb'),
-		notify	=> Service['nginx'],
-	}
-	file { '/etc/motd' :
-        	content => "Enjoy the server =)\nPUoppet is on the control here\n",
-	}
-}# Fim da reclaraçao dos recursos de puppet-client2
+
+node 'puppet-client2' {
+
+include base
+
+}
--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -9,29 +9,29 @@ include ssh
 include sudoers
 package { 'firewalld':
 	ensure	=> present,
-	}
+}
 service	{ 'firewalld':
 	ensure	=> running,
 	enable	=> true,
 	require	=> Package['firewalld'],
-	}
+}
 package	{ 'iptables-services':
 	ensure	=> absent,
-	}
+}
 file 	{ '/etc/hosts':
 	ensure	=> present,
 	content	=> template('base/hosts.conf.erb'),
 	mode	=> 0664,
 	owner	=> 'root',
 	group	=> 'root',
-	}
+}
 file 	{ '/etc/resolv.conf':
 	ensure	=> present,
 	source	=> 'puppet:///modules/base/resolv.conf',
 	mode 	=> 0664,
 	owner	=> 'root',
 	group	=> 'root',
-	}
+}
 #file 	{ '/etc/yum.repos.d/repositorio.repo':
 #	ensure 	=> present,
 #	source	=> 'puppet:///modules/base/repositorio.repo',
@@ -45,16 +45,47 @@ file	{ '/etc/nsswitch.conf':
 	mode	=> 0664,
 	owner	=> 'root',
 	group	=> 'root',	
-	}
+}
 file	{ '/var/git/puppet/.git/config':
 	ensure	=> present,
 	source	=> 'puppet:///modules/base/git.config',
 	mode	=> 0664,	
 	owner	=> 'git',
 	group	=> 'git',
-	}	
+}	
 } 
 default:  { notify{ 'Sistema operacional é Debian': }
-	}
-	}
+}
+
+}
+if $ipaddress =~ /192.168.*/ {
+        $environment = 'production'
+        notify  { "Ambiente = $environment": }
+}
+elsif $ipaddress =~ /172.16.*/ {
+        $environment = 'stagging'
+        notify  { "Ambiente = $environment": }
+}
+else {
+        $environment = 'dev'
+        notify  { "Ambiente = $environment": }
+}
+if $hostname =~ /web-*/ {
+        include nginx
+}
+else { notify { "Hostname = $hostname": }
+}
+user	{ 'admin':
+	ensure	=> present,
+	home	=> '/home/admin',
+	managehome	=> true,
+	comment	=> 'Usuário administrador padrão',
+	password	=> '$1$RG5eUKfI$FGFofQoEqyj4GErReMCtg/',
+}
+ssh_authorized_Key	{ 'admin':
+	type	=> 'rsa',
+	key	=> "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpwAhX/pnIc3V66wU/nOlCwwI2iuB5E/+JE8+t11daeUOXEWTcS1/XStpXYCbJmALS+xg1TznRsUf1VFSdjgdlfKBCXyyvY/eiwDHz24CmqNLmBlNL6BSYnqTj5iPcR8eOWt4NZgu9G4tfOScLez2CsdIEKvO60ZnkFjPOZ5Wm+WvXbP7z0FGqBY2rwVxc7T0EYloafeZR6/aAeUkvA6DHrgIRyAKBp3cyT+M6nCU5whtWt1B062PtDdpX/VCTxdPqNCZaD1uVmG55yA1SC8B4iLABTv2V6RXkhRpnZUekfMs+i1SLl/st4rUOAP2BVTFFeyApzVqadmlTkvZtHsy1",
+	user	=> "$name",
+}
+
 }
